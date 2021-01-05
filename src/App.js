da8ffc067c61e69bfa011006149025eace5dc86f
karoltomaszewski <karoltomaszewski@users.noreply.github.com>
2021-01-06 00:33:49 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Images
+import BicycleImg from './Images/bicycle.svg';
+import CheeseImg from './Images/cheese.svg';
+import TomatoImg from './Images/tomato.svg';
+
+import Card from './Components/Card';
+
+class App extends Component{
+  state = {
+    words: [
+      {polish: "ser", english: "cheese", img: CheeseImg, text: "", done: "no"},
+      {polish: "rower", english: "bicycle", img: BicycleImg, text: "", done: "no"},
+      {polish: "pomidor", english: "tomato", img: TomatoImg, text: "", done: "no"}
+    ]
+  }
+
+  onChangeHandler = (event, id) =>{
+    const words = [...this.state.words];
+
+    const word = {...this.state.words[id]};
+    word.text = event.target.value;
+
+    if(event.target.value === this.state.words[id].polish){
+      word.done = "yes";
+    }
+
+    words[id] = word;
+
+    this.setState({words: words});
+  }
+
+  onClickHandler = (event, id) =>{
+    const words = [...this.state.words];
+
+    const word = {...this.state.words[id]};
+
+    word.done = "shown";
+
+    words[id] = word;
+
+    this.setState({words: words});
+  }
+
+
+  render(){
+
+
+    return (
+      <div className="App">
+        <h1>Learning cards</h1>
+        <div className="CardsBox">
+          {
+            this.state.words.map((word, index) =>{
+              return(
+                <Card 
+                  key={word.english}
+                  english={word.english} 
+                  img={word.img}
+                  done={word.done}
+                  polish={word.polish}
+                  change={event => this.onChangeHandler(event, index)}
+                  click={event => this.onClickHandler(event, index)}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,24 @@
+const Card = props =>{
+    let polishPart = null;
+    if(props.done === "yes" || props.done === "shown"){
+        polishPart = <h3>{props.polish}</h3>
+    }else if(props.done === "no"){
+        polishPart = (
+        <div>
+            <input onChange={props.change} placeholder="Enter in Polish what it is"></input>
+            <button onClick={props.click}>Show me answer</button>
+         </div>
+        )
+    }
+
+    console.log(props.done)
+
+    return(
+        <div className={"Card done_"+props.done}>
+            <img src={props.img} alt={props.english}/>
+            {polishPart}
+        </div>
+    );
+}
+
+export default Card;
